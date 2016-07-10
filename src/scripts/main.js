7224970ce9c66dbcfed921fd7a884f738ae99599
Andres Sainz de aja <andressainz@gmail.com>
2016-07-10 14:07:45 -0300

    basic checkbox
    add a basic checkbox that stores the state in localStorage and retrieve it form there

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
-let hello = 'World!';
+import xs from 'xstream';
+import {run} from '@cycle/xstream-run';
+import {makeDOMDriver, div, input, p} from '@cycle/dom';
+
+function main(sources){
+    const sinks = {
+        DOM: sources.DOM.select('input').events('change')
+            .map(ev => ev.target.checked)
+            .startWith(JSON.parse(localStorage.getItem('cyclejs.todo.sample-checked')) || false)
+            .map(checked => {
+                window.localStorage.setItem('cyclejs.todo.sample-checked', checked);
+                return div([
+                    input({
+                        attrs: {type: 'checkbox', checked: checked},
+                    }), 'toggle me', p(checked ? 'ON': 'OFF')
+                ]);
+            })
+    };
+
+    return sinks;
+}
+
+const drivers = {
+    DOM: makeDOMDriver('#app')
+};
+
+run(main, drivers);
